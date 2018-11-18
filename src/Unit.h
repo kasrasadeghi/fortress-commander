9a4eee263e373b15399931dd234729f6d0c0b91a
kasrasadeghi <kasra.sadeghi1@gmail.com>
2018-11-18 00:07:10 -0600

    remove unittype comments (for now)

--- src/Unit.h
+++ src/Unit.h
@@ -2,19 +2,17 @@
 
 #include <SFML/Graphics.hpp>
 
-// enum class UnitType { FRIENDLY, HOSTILE };
 
 class Unit : public sf::Drawable {
   sf::Vector2f _pos;
-  // UnitType _type; // friendly or hostile
   sf::Vector2f _target;
 
 public:
   constexpr static float unit_size = 0.5f;
   constexpr static float unit_speed = 2.f;
 
-  Unit(sf::Vector2f pos /*, UnitType type*/)
-      : _pos(pos), _target(pos) /*, _type(type)*/
+  Unit(sf::Vector2f pos)
+      : _pos(pos), _target(pos)
   {}
 
   virtual void draw(sf::RenderTarget& rw, sf::RenderStates states) const;
