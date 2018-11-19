a42ff0f1a10db71668142906cabaa0717373fe64
kasrasadeghi <kasra.sadeghi1@gmail.com>
2018-11-19 14:40:53 -0600

    extract makeTriangle

--- src/Game.cpp
+++ src/Game.cpp
@@ -22,6 +22,26 @@ Game::Game()
   // if (!_font.loadFromFile("arial.ttf")) { exit(1); }
 }
 
+GLuint makeTriangle(float* vertices, size_t size) {
+  GLuint VBO, VAO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
+
+  // position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+  // color attribute
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+  return VAO;
+}
+
 void Game::loop() {
   Shader shader("shaders/triangle.vs", "shaders/triangle.fs");
   float vertices[] = {
@@ -44,21 +64,10 @@ void Game::loop() {
   //    0.3f, -0.7f, 0.0f, 0.0f, 1.0f, 0.0f,
   //   -0.7f,  0.7f, 0.0f, 0.0f, 0.0f, 1.0f,
   // };
-  unsigned int VBO, VAO;
-  glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &VBO);
-  // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-  glBindVertexArray(VAO);
+  
 
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-  // position attribute
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-  glEnableVertexAttribArray(0);
-  // color attribute
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-  glEnableVertexAttribArray(1);
+  GLuint VAO = makeTriangle(vertices, sizeof(vertices));
+ 
 
 
   while (_window.isOpen()) {
