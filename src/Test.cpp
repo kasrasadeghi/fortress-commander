3d3f1c98ab1f1faf2e7bbfbf7062ebdecf68efda
kasrasadeghi <kasra.sadeghi1@gmail.com>
2018-11-21 03:51:07 -0600

    more testing. shape should work now.
    TODO:
    need to engineer some friend class solution

--- src/Test.cpp
+++ src/Test.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h> //TODO
 
+#include <glm/gtc/matrix_transform.hpp>
+
 #include "Game.h"
 #include "Graphics.h"
 
@@ -27,30 +29,35 @@ TEST(Shape, RectBuilder_Value) {
   GLfloat rotate(0);
 
   EXPECT_EQ(r._position, position);
+  EXPECT_EQ(r._size, size);
+  EXPECT_EQ(r._rotate, rotate);
+
   //TODO make _position and other shape things private and use a viewer friend with a #define or something
 }
   
+TEST(Shape, model) {
+  RectangleShape r;
+  r.position(90, 90).size(20, 20);
 
-  // auto& v = r._base_vertices;
-  // auto a = glm::vec2(v[0], v[1]);
+  glm::vec2 position(90, 90);
+  glm::vec2 size(20, 20);
+  GLfloat rotate(0);
+  
+  glm::mat4 m(1);
+  m = glm::translate(m, glm::vec3(position, 0.f));
 
-  // /* model test */ {
-  //   glm::mat4 m(1);
-  //   m = glm::translate(m, glm::vec3(position, 0.f));
+  m = glm::translate(m, glm::vec3(position[0] *  .5f, position[1] *  .5f, 0.f));
+  m = glm::rotate(m, rotate, glm::vec3(0.f, 0.f, 1.f));
+  m = glm::translate(m, glm::vec3(position[0] * -.5f, position[1] * -.5f, 0.f));
 
-  //   m = glm::translate(m, glm::vec3(position[0] *  .5f, position[1] *  .5f, 0.f));
-  //   m = glm::rotate(m, rotate, glm::vec3(0.f, 0.f, 1.f));
-  //   m = glm::translate(m, glm::vec3(position[0] * -.5f, position[1] * -.5f, 0.f));
+  m = glm::scale(m, glm::vec3(size, 1.f));
 
-  //   m = glm::scale(m, glm::vec3(size, 1.f));
+  r.model();
+  EXPECT_EQ(m, r._model);
+}
 
-  //   r.model();
-  //   EXPECT_EQ(m, r._model);
-  // } 
-// }
-
-// TEST(Shape, view) {
-//   auto view = View().center(100.f, 100.f).radius(20.f, 20.f);
-//   auto view_check = glm::ortho(80.f, 120.f, 80.f, 120.f, -1.f, 1.f);
-//   EXPECT_EQ(view.proj(), view_check);
-// }
+TEST(Shape, view) {
+  auto view = View().center(100.f, 100.f).radius(20.f, 20.f);
+  auto view_check = glm::ortho(80.f, 120.f, 80.f, 120.f, -1.f, 1.f);
+  EXPECT_EQ(view.proj(), view_check);
+}
