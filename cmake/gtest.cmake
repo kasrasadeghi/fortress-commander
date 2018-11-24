acf889e52db81bd9389700b776f23267adaa9560
kasrasadeghi <kasra.sadeghi1@gmail.com>
2018-11-24 17:49:28 -0600

    link with freetype and fix gtest.cmake

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -1,11 +1,5 @@
 function(setup_gtest executable)
   find_package(GTest REQUIRED)
-  message(STATUS "GTest libraries found: " ${SFML_LIBRARIES})
-  message(STATUS "GTest include dir found: " ${SFML_INCLUDE_DIR})
-
-  target_include_directories(${executable} PUBLIC
-    ${SFML_INCLUDE_DIR}
-  )
 
   target_link_libraries(${executable} PUBLIC
     gtest
