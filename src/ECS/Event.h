793708d67b53ef0021195867db7c886ca2d4bf84
Austin Atchley <austinatchley@gmail.com>
2018-11-28 01:43:51 -0600

    Cleaned up EventManager and Event.h

--- src/ECS/Event.h
+++ src/ECS/Event.h
@@ -13,48 +13,36 @@
 #include <vector>
 
 namespace ECS {
-class EventManager;
 class BaseEventSubscriber;
-class BaseEvent;
 
 using TypeIndex = std::type_index;
 
 using SubscribersMap = std::unordered_map<
     TypeIndex,
-    std::vector<std::shared_ptr<BaseEventSubscriber>>,
-    std::hash<TypeIndex>, std::equal_to<TypeIndex>>;
-using EventQueue = std::unordered_map<BaseEventSubscriber*, unsigned>;
+    std::vector<std::shared_ptr<BaseEventSubscriber>>>;
 
 template <typename T> TypeIndex getTypeIndex() {
   return std::type_index(typeid(T));
 }
 
-/////////////////////////////////////////////////////////////////
-class BaseEvent {
-public:
-  typedef std::size_t Family;
-
-  virtual ~BaseEvent() {}
-
-protected:
-  static Family mFamilyCounter;
-};
-
-template <typename Derived> class Event : public BaseEvent {
-public:
-  static Family family() {
-    static Family family = mFamilyCounter++;
-    return family;
-  }
-};
-
-/////////////////////////////////////////////////////////////////
+/**
+ * @brief The base class for EventSubscribers to inherit from
+ * @detail Pointers to BaseEventSubscribers are stored in _subscribers,
+ * the internal subscriber storage.
+ */
 class BaseEventSubscriber {
 public:
   virtual ~BaseEventSubscriber() {}
 };
 
-/////////////////////////////////////////////////////////////////
+/**
+ * @brief Templated BaseEventSubscriber
+ * @detail Allows the user to simply provide the EventSubscriber class
+ * with a templated type of event, and gives the user a `receive` function
+ * to handle the event in.
+ *
+ * @tparam Event The type of event this subscriber should handle
+ */
 template <typename Event> class EventSubscriber : public BaseEventSubscriber {
 public:
   virtual ~EventSubscriber() {}
@@ -62,11 +50,13 @@ public:
   virtual void receive(const Event& event) = 0;
 };
 
-/////////////////////////////////////////////////////////////////
+/**
+ * @brief The singleton manager of all events being sent or received
+ * @detail Takes events, queues them, and routes them to all connected
+ * subscribers on call of update.
+ */
 class EventManager {
-  EventManager()
-      : mEntities({}),
-        mSubscribers({}, 0, std::hash<TypeIndex>(), std::equal_to<TypeIndex>()) {}
+  EventManager() {}
 
   EventManager(const EventManager&) = delete;
   void operator=(const EventManager&) = delete;
@@ -79,26 +69,38 @@ class EventManager {
       return instance;
   }
 
+  /**
+   * @brief Connects the subsciber to the channel of Events being handled by the EventManager
+   *
+   * @tparam Event The type of events the subscriber receives
+   * @param subscriber The instance of EventSubscriber with the Event template parameter
+   */
   template <typename Event> void _connect(EventSubscriber<Event>* subscriber) {
     auto index = getTypeIndex<Event>();
-    auto it = mSubscribers.find(index);
+    auto it = _subscribers.find(index);
 
-    if (it == mSubscribers.end()) {
+    if (it == _subscribers.end()) {
       std::vector<std::shared_ptr<BaseEventSubscriber>> subList;
       subList.push_back(std::shared_ptr<BaseEventSubscriber>(subscriber));
 
-      mSubscribers.insert({index, subList});
+      _subscribers.insert({index, subList});
     } else {
       it->second.push_back(std::shared_ptr<BaseEventSubscriber>(subscriber));
     }
   }
 
+  /**
+   * @brief Disonnects the subsciber from the channel of Events being handled by the EventManager
+   *
+   * @tparam Event The type of events the subscriber receives
+   * @param subscriber The instance of EventSubscriber with the Event template parameter
+   */
   template <typename Event>
   void _disconnect(EventSubscriber<Event>* subscriber) {
     auto index = getTypeIndex<Event>();
-    auto it = mSubscribers.find(index);
+    auto it = _subscribers.find(index);
 
-    if (it != mSubscribers.end()) {
+    if (it != _subscribers.end()) {
       auto secondIndex = it->second.begin();
       while (secondIndex != it->second.end() &&
              secondIndex->get() != subscriber) {
@@ -106,52 +108,42 @@ class EventManager {
       }
       if (secondIndex != it->second.end()) { it->second.erase(secondIndex); }
 
-      if (it->second.size() == 0) { mSubscribers.erase(it); }
+      if (it->second.size() == 0) { _subscribers.erase(it); }
     }
   }
 
+  /**
+   * @brief Dispatches every queued event stored in _events
+   */
   void _update() {
-    if (mEvents.empty()) { return; }
+    for (auto event : _events) { event(); }
 
-    for (auto event : mEvents) { event(); }
-
-    mEvents.clear();
+    _events.clear();
   }
 
-  // After the data structure for subscribers is modified, this should take an
-  // ID
-  // so that we can subscribe to multiple channels for the same event
+  
+  /**
+   * @brief Give the EventManager an event to emit to all connected subscribers
+   *
+   * @tparam Event Only connected subscribers with the template parameter Event
+   * will receive the event.
+   * @param event The instance of Event that should be sent to each connected subscriber.
+   */
   template <typename Event> void _event(const Event* event) {
-    auto it = mSubscribers.find(getTypeIndex<Event>());
+    auto it = _subscribers.find(getTypeIndex<Event>());
 
-    if (it != mSubscribers.end()) {
+    if (it != _subscribers.end()) {
       const auto& subscribers = it->second;
       for (auto& base : subscribers) {
         auto sub = reinterpret_cast<EventSubscriber<Event>*>(base.get());
 
         const auto boundFunc =
             std::bind(&EventSubscriber<Event>::receive, sub, *event);
-        mEvents.push_back(std::function<void()>(boundFunc));
+        _events.push_back(std::function<void()>(boundFunc));
       }
     }
     delete event;
   }
-
-  // template <typename Event> void broadcast(Event& event) {}
-
-  // template <typename Result, typename Event>
-  // void eventResult(Result* res, Entity id, const Event& event);
-
-  // template <typename Result, typename Event>
-  // void broadcastResult(Result* res, const Event& event);
-
-  // template <typename Event>
-  // void sendEvent(EventManager* eventManager, const Event& event,
-  //                BaseEventSubscriber* base) {
-  //   auto* sub = reinterpret_cast<EventSubscriber<Event>*>(base);
-
-  //   sub->receive(event);
-  // }
 
 public:
   template <typename Event> static void connect(EventSubscriber<Event>* subscriber) {
@@ -169,11 +161,8 @@ public:
   }
 
 private:
-  std::vector<Entity*> mEntities;
+  SubscribersMap _subscribers;
 
-  SubscribersMap mSubscribers;
-
-  EventQueue mEventQueue;
-  std::vector<std::function<void()>> mEvents;
+  std::vector<std::function<void()>> _events;
 };
 } // namespace ECS
--- src/Events.h
+++ src/Events.h
@@ -2,24 +2,24 @@
 
 #include "ECS/Event.h"
 
-struct MouseMoveEvent : public ECS::BaseEvent {
+struct MouseMoveEvent {
   MouseMoveEvent(float x, float y) : x(x), y(y) {}
   float x, y;
 };
 
-struct MouseDownEvent : public ECS::BaseEvent {
+struct MouseDownEvent {
   MouseDownEvent(int button, float x, float y) : button(button), x(x), y(y) {}
   int button;
   float x, y;
 };
 
-struct MouseUpEvent : public ECS::BaseEvent {
+struct MouseUpEvent {
   MouseUpEvent(int button, float x, float y) : button(button), x(x), y(y) {}
   int button;
   float x, y;
 };
 
-struct KeyEvent : public ECS::BaseEvent {
+struct KeyEvent {
   KeyEvent(int key, int action) : key(key), action(action) {}
   int key;
   int action;
