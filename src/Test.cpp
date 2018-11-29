71b6d50f3c429792899bf46538c8095dd261c038
kasrasadeghi <kasra.sadeghi1@gmail.com>
2018-11-28 23:21:46 -0600

    mirror aliveSet for seen()

--- src/Test.cpp
+++ src/Test.cpp
@@ -7,33 +7,40 @@
 
 #include "Game.h"
 #include "Graphics.h"
+#include "World.h"
 
-Game g; // sets up opengl
+// Game g; // sets up opengl
 
-TEST(Construction, Shader) {
-  const Shader _shader{"triangle"};
-}
+// TEST(Construction, Shader) {
+//   const Shader _shader{"triangle"};
+// }
 
-TEST(Shape, view) {
-  auto view = View().center(100.f, 100.f).radius(20.f, 20.f);
-  // sfml default view where 0,0 is top left
-  auto view_check = glm::ortho(80.f, 120.f, 120.f, 80.f, -1.f, 1.f);
-  EXPECT_EQ(view.proj(), view_check);
-}
+// TEST(Shape, view) {
+//   auto view = View().center(100.f, 100.f).radius(20.f, 20.f);
+//   // sfml default view where 0,0 is top left
+//   auto view_check = glm::ortho(80.f, 120.f, 120.f, 80.f, -1.f, 1.f);
+//   EXPECT_EQ(view.proj(), view_check);
+// }
 
-TEST(Shape, pi) {
-  using namespace std;
-  cout << glm::pi<float>() << endl;
-  cout << glm::half_pi<float>() << endl;
-}
+// TEST(Shape, pi) {
+//   using namespace std;
+//   cout << glm::pi<float>() << endl;
+//   cout << glm::half_pi<float>() << endl;
+// }
 
-TEST(Shape, circle) {
-  using namespace std;
-  uint size_count = 4;
-  for (uint i = 0; i < size_count; ++i) {
-    float angle = i * 2 * glm::pi<float>() / size_count;
-    float x = cos(angle);
-    float y = sin(angle);
-    cout << angle << ": " << x << ", " << y << endl;
-  }
+// TEST(Shape, circle) {
+//   using namespace std;
+//   uint size_count = 4;
+//   for (uint i = 0; i < size_count; ++i) {
+//     float angle = i * 2 * glm::pi<float>() / size_count;
+//     float x = cos(angle);
+//     float y = sin(angle);
+//     cout << angle << ": " << x << ", " << y << endl;
+//   }
+// }
+
+TEST(Pathing, findPathSpeed) {
+  size_t world_size = World::world_size;
+  Region region {{world_size, std::vector<Tile>(world_size, Tile::GRASS)}};
+  MoveSystem::findPath(region, {0, 0}, {world_size - 1, world_size - 1});
 }
