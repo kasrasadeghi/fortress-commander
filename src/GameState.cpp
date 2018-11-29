562b91b08a8f21dd701427466e2cb29d52542e82
kasrasadeghi <kasra.sadeghi1@gmail.com>
2018-11-28 19:00:19 -0600

    center view on world center

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -2,6 +2,8 @@
 #include "Game.h"
 
 GameState::GameState(RenderWindow& window) : _window(window) {
-  _view.center(Game::view_size / 2.f * _window.widthScalingFactor(), Game::view_size / 2.f)
-      .radius(Game::view_size / 2.f * _window.widthScalingFactor(), Game::view_size / 2.f);
+  _view
+    // .center(Game::view_size / 2.f * _window.widthScalingFactor(), Game::view_size / 2.f)
+    .center(World::world_size / 2.f, World::world_size / 2.f)
+    .radius(Game::view_size / 2.f * _window.widthScalingFactor(), Game::view_size / 2.f);
 }
