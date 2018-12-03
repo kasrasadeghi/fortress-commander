4c7bdf9306080ab42170159198e33435ec4f7bab
kasrasadeghi <kasra.sadeghi1@gmail.com>
2018-12-03 05:37:25 -0600

    actually update resource manager

--- src/Graphics/ResourceManager.cpp
+++ src/Graphics/ResourceManager.cpp
@@ -1,17 +1,12 @@
 #include "ResourceManager.h"
 #include <string>
 
-<<<<<<< HEAD
-std::array<Shader, 2> ResourceManager::_shaders = {std::string("instanced_array"),
-                                                   std::string("text")};
-=======
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 std::array<Shader, 3> ResourceManager::_shaders = {std::string("instanced_array"),
                                                    std::string("text"),
                                                    std::string("texture")};
->>>>>>> 1ad5d65... remove texture2D code from resourcemanager
 
 Shader& ResourceManager::getShader(SHADER_INDEX i) {
   Shader& s = _shaders[static_cast<std::size_t>(i)];
